e88a4b4cdba34bf784dfbe8452e01d4495de7576
Robert Piwowarek <robpiwowarek@gmail.com>
2019-07-17 17:31:49 +0200

    initial file upload

---
+++ Assets/Scripts/Shooting/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rigidbody;
+    public float damage = 20f;
+
+    private void Start()
+    {
+        rigidbody.velocity = transform.right * speed;
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Health enemy = other.collider.GetComponent<Health>();
+        
+        if (enemy)
+        {
+            if (!enemy.IsDead)
+            {
+                Debug.Log(enemy.IsDead);
+                enemy.takeDamage(damage);
+
+            }
+        }
+        
+        Destroy(gameObject);
+    }
+}

c8799edadad68c356a87e45735ee21d8d11e1d02
Hubert Lipiński <hlp@touk.pl>
2019-07-19 17:47:59 +0200

    Camera follow Camera resize Input controller added

---
+++ Assets/Scripts/Camera/CameraCenterFollower.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraCenterFollower : MonoBehaviour
+{
+    public Transform player1Transform;
+    public Transform player2Transform;
+    public Transform cameraTransform;
+    
+    private Vector3 _cameraPosition;
+    private float _time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraPosition = cameraTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _cameraPosition = cameraTransform.position;
+        Vector3 centerPoint = (player1Transform.position + player2Transform.position) / 2;
+        var cameraX = _cameraPosition.x;
+        var cameraY = _cameraPosition.y;
+        _time += Time.deltaTime;
+        cameraTransform.localPosition = new Vector3(
+            Mathf.SmoothStep(cameraX, centerPoint.x, _time),
+            Mathf.SmoothStep(cameraY, centerPoint.y, _time),
+            _cameraPosition.z
+        );
+    }
+}
---
+++ Assets/Scripts/Camera/CameraResizer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraResizer : MonoBehaviour
+{
+    public float minSize = 3.0f;
+    public float maxSize = 6.5f;
+    public float epsilon = 0.1f;
+    public Transform player1Transform;
+    public Transform player2Transform;
+
+    private Camera _camera;
+    private float _time = 0;
+    
+    private const float EpsilonExpandX = 4f;
+    private const float EpsilonCollapseX = 6f;
+    private const float EpsilonExpandY = 2f;
+    private const float EpsilonCollapseY = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distanceX = Math.Abs(player1Transform.position.x - player2Transform.position.x);
+        var distanceY = Math.Abs(player1Transform.position.y - player2Transform.position.y);
+        var cameraSize = _camera.orthographicSize;
+        var viewPointWidth = _camera.aspect * 2f * cameraSize;
+        var viewPointHeight = 2f * cameraSize;
+        if (distanceX > viewPointWidth - EpsilonExpandX)
+        {
+            if (cameraSize > maxSize) return;
+            _time += Time.deltaTime;
+            _camera.orthographicSize = Mathf.SmoothStep(_camera.orthographicSize, cameraSize + epsilon, _time);
+        }
+        else if (distanceX < viewPointWidth - EpsilonCollapseX && viewPointHeight - distanceY > EpsilonCollapseY)
+        {
+            if (cameraSize < minSize) return;
+            _time += Time.deltaTime;
+            _camera.orthographicSize = Mathf.SmoothStep(_camera.orthographicSize, cameraSize - epsilon, _time);
+        }
+        else if (distanceY > viewPointHeight - EpsilonExpandY)
+        {
+            if (cameraSize > maxSize) return;
+            _time += Time.deltaTime;
+            _camera.orthographicSize = Mathf.SmoothStep(_camera.orthographicSize, cameraSize + epsilon, _time);
+        }
+        else if (distanceY < viewPointHeight - EpsilonCollapseY && viewPointWidth - distanceX > EpsilonCollapseX)
+        {
+            if (cameraSize < minSize) return;
+            _time += Time.deltaTime;
+            _camera.orthographicSize = Mathf.SmoothStep(_camera.orthographicSize, cameraSize - epsilon, _time);
+        }
+    }
+}

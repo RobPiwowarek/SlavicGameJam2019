f391a8f48c185bf502877f25db2a9df4815260c7
Daniel Król <dkr@touk.pl>
2019-07-19 17:08:01 +0200

    Add spawn buildings

---
+++ Assets/Scripts/Building/Builder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    public float price;
+    public BasicBuild build;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetButton("ActionButton") && gameObject.transform.childCount < 1)
+        {
+            var basicBuild = Instantiate ( build, gameObject.transform );
+            basicBuild.transform.parent = gameObject.transform;
+        }
+    }
+}

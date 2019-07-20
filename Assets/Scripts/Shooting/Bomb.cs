98b5c7cf83811585d30af868222866c3386ce873
Maja Jabłońska <majajjablonska@gmail.com>
2019-07-20 19:44:43 +0200

    Splash damage for bombs

--- Assets/Scripts/Shooting/Bomb.cs
+++ Assets/Scripts/Shooting/Bomb.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class Bomb : Bullet
 {
+    [SerializeField] private float splashRadius;
+    [SerializeField] private float damageModifier;
+    protected override void applyDamage(Collision2D other)
+    {
+        LayerMask mask = LayerMask.GetMask("Default");
+        List<Collider2D> hitColliders = Physics2D.OverlapCircleAll(other.transform.position, splashRadius, mask).ToList();
+        
+        hitColliders.ForEach(c => c.GetComponent<AttackTarget>()?.ReceiveDamage((int)(damageModifier * damage)));
+            
+    }
 }

deb831f9b33b086fe7a38b9a3a623fc167f30e4d
agatak8 <shnutzer@gmail.com>
2019-07-20 10:49:18 +0200

    build slot

---
+++ Assets/Scripts/Building/BuildSlot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BuildSlot : MonoBehaviour
+{
+    public Rigidbody2D Buildable;
+
+    bool IsTaken()
+    {
+        return this.transform.childCount > 0;
+    }
+
+    void SpawnChild()
+    {
+        if (this.IsTaken())
+        {
+            Debug.LogError("This build slot already has a building");
+        }
+        else if (!Buildable)
+        {
+            Debug.LogError("Buildable not set");
+        }
+        else
+        {
+            Debug.Log("Spawning building");
+            Rigidbody2D p = Instantiate(Buildable, this.transform.position, this.transform.rotation, this.transform);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}

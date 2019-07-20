5aed4fda80153668e682842090c65eb654c57910
Daniel Król <dkr@touk.pl>
2019-07-20 11:36:21 +0200

    Upgrade Builds

---
+++ Assets/Scripts/Building/BuildData.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class BuildLevel
+{
+    public int cost;
+    public GameObject visualization;
+    public float damage;
+    public float maxHealth;
+}
+
+public class BuildData : MonoBehaviour
+{
+
+    public List<BuildLevel> levels;
+    private BuildLevel currentLevel;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //1
+    public BuildLevel CurrentLevel
+    {
+        //2
+        get
+        {
+            return currentLevel;
+        }
+        //3
+        set
+        {
+            currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(currentLevel);
+
+            GameObject levelVisualization = levels[currentLevelIndex].visualization;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levelVisualization != null)
+                {
+                    if (i == currentLevelIndex)
+                    {
+                        levels[i].visualization.SetActive(true);
+                    }
+                    else
+                    {
+                        levels[i].visualization.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        CurrentLevel = levels[0];
+    }
+
+    public BuildLevel GetNextLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        int maxLevelIndex = levels.Count - 1;
+        if (currentLevelIndex < maxLevelIndex)
+        {
+            return levels[currentLevelIndex + 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void IncreaseLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        if (currentLevelIndex < levels.Count - 1)
+        {
+            CurrentLevel = levels[currentLevelIndex + 1];
+        }
+    }
+
+}
---
+++ Assets/Scripts/Building/Upgrader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrader : MonoBehaviour
+{
+    public float cost;
+
+    public float costMultipler;
+
+    public int maxUpgrade;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        
+    }
+}

044e46c02b692cc4a5c57b2eae97e41a5a8e8565
Maja Jabłońska <majajjablonska@gmail.com>
2019-07-20 11:59:23 +0200

    Rotation of enemies: looking at attack target

--- Assets/Scripts/Enemy/Movement/FollowingEnemy.cs
+++ Assets/Scripts/Enemy/Movement/FollowingEnemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Quaternion = System.Numerics.Quaternion;
 
 public class FollowingEnemy : Movement
 {
@@ -6,28 +7,48 @@ public class FollowingEnemy : Movement
     [SerializeField] protected float distanceBuffer;
     [SerializeField] protected float speed;
 
-    public override Vector3 Move(Vector3 current)
+    public override Vector2 Move(Vector2 current)
     {
         if (!attackTarget) return current;
+
+        Vector2 targetPosition = attackTarget.transform.position;
         
-        if ((attackTarget.transform.position - current).sqrMagnitude > minimumDistance)
+        if ((((Vector2)attackTarget.transform.position - (Vector2)current)).sqrMagnitude > minimumDistance)
         {
-            return Vector3.MoveTowards(
+            Vector2 result = Vector2.MoveTowards(
                 current,
                 this.attackTarget.transform.position,
                 speed * Time.fixedDeltaTime
             );
+
+            return result;
         }
         
-        if ((attackTarget.transform.position - current).sqrMagnitude < minimumDistance)
+        if (((Vector2)attackTarget.transform.position - (Vector2)current).sqrMagnitude < minimumDistance)
         {
-            return Vector3.MoveTowards(
+            Vector2 result = Vector2.MoveTowards(
                  current,
                 this.attackTarget.transform.position,
                 -1 * speed * Time.fixedDeltaTime
             );
+
+            return result;
         }
 
         return current;
+    }
+
+    public override UnityEngine.Quaternion Rotate(Vector2 current)
+    {
+        if (attackTarget)
+        {
+
+            UnityEngine.Quaternion newRotation = UnityEngine.Quaternion.LookRotation((Vector2) attackTarget.transform.position- current, Vector3.up);
+            newRotation.x = 0.0f;
+            newRotation.y = 0.0f;
+
+            return newRotation;
+        }
+        else return UnityEngine.Quaternion.identity;
     }
 }

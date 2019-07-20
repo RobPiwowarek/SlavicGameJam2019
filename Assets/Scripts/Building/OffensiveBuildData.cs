311a9d8db317e978a7fd70a8b506cf5ffce32ecf
Maja Jabłońska <majajjablonska@gmail.com>
2019-07-20 17:59:32 +0200

    Offensive Building

--- Assets/Scripts/Building/OffensiveBuildData.cs
+++ Assets/Scripts/Building/OffensiveBuildData.cs
@@ -1,6 +1,8 @@
+using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class OffensiveBuildData : MonoBehaviour
 {
@@ -8,16 +10,64 @@ public class OffensiveBuildData : MonoBehaviour
     [SerializeField] private GameObject bullet;
     [SerializeField] private float seconds;
     [SerializeField] private Transform firePoint;
+    [SerializeField] private AttackTarget attackTarget;
+    [SerializeField] private float searchRadius;
+
+    private bool facedRight;
     void Start()
     {
         StartCoroutine(CheckIfTimePassed());
+        facedRight = firePoint.position.x > this.transform.position.x;
     }
-    
+
+    private void FixedUpdate()
+    {
+        if (!attackTarget)
+        {
+            this.attackTarget = searchForEnemy();
+            return;
+        }
+            UnityEngine.Quaternion newRotation = UnityEngine.Quaternion.LookRotation(
+                attackTarget.transform.position - this.transform.position,
+                Vector3.up
+            );
+
+            newRotation.x = 0.0f;
+            newRotation.y = 0.0f;
+
+            this.transform.rotation = newRotation;
+            
+            if (attackTarget)
+            {
+                if (this.transform.position.x > attackTarget.transform.position.x)
+                {
+                    transform.Rotate(0f, 180f, 0f);
+                }
+                else
+                {
+                    transform.Rotate(0f, 0f, 0f);
+                }
+            }
+
+    }
+
     IEnumerator CheckIfTimePassed() {
         for(;;)
         {
-            Instantiate(bullet, firePoint.position, firePoint.rotation).GetComponent<Transform>();
+            if (attackTarget) Instantiate(bullet, firePoint.position, firePoint.rotation).GetComponent<Transform>();
             yield return new WaitForSeconds(seconds);
         }
+    }
+
+    private AttackTarget searchForEnemy()
+    {
+        LayerMask mask = LayerMask.GetMask("Default");
+        List<Collider2D> colliders = Physics2D.OverlapCircleAll(this.transform.position, searchRadius, mask)
+            .Where(o => o.CompareTag("Enemy"))
+            .OrderBy(o => (o.transform.position - this.transform.position).sqrMagnitude)
+            .ToList();
+
+        if (colliders.Count > 0) return colliders[0].GetComponent<AttackTarget>();
+        return null;
     }
 }

33522591463841a3c7c42f7dd098f4de5efe725e
agatak8 <shnutzer@gmail.com>
2019-07-20 10:37:54 +0200

    Repair range + script

---
+++ Assets/Scripts/Repair.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repair : MonoBehaviour
+{
+    public float restorePoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("Repair key pressed");
+            Health otherHealth = other.GetComponent<Health>();
+            if (otherHealth)
+            {
+                Debug.Log("Restoring health");
+                otherHealth.restoreHealth(restorePoints);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}

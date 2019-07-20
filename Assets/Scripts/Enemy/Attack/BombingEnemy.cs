fa39f98a4b17b24c583f7487088f5a61dc35938e
Maja Jabłońska <majajjablonska@gmail.com>
2019-07-20 12:44:51 +0200

    Bombing enemies

---
+++ Assets/Scripts/Enemy/Attack/BombingEnemy.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class BombingEnemy : PeriodicallyShootingEnemy
+{
+    public override void Attack()
+    {
+        AttackTarget attackTarget= this.GetComponentInParent<Enemy>().attackTarget;
+        
+        if (attackTarget)
+        {
+            float distance = Math.Abs(attackTarget.transform.position.x -
+                                      this.firePoint.transform.position.x);
+        
+            if (fire && distance < 1.0f)
+            {
+                Debug.Log("BOMB");
+                // TODO: drop bomb only above tower or something
+                Transform obj = Instantiate(bullet, firePoint.position, firePoint.rotation).GetComponent<Transform>();
+            }
+        
+            fire = false;
+        }
+    }
+}

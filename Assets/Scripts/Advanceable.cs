bbc0b615c3b570b9280513978b188854b105a498
Robert Piwowarek <rpw@touk.pl>
2019-07-20 04:20:07 +0200

    tree poc

---
+++ Assets/Scripts/Advanceable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Advanceable : MonoBehaviour
+{
+    public List<Advanceable> nextForms;
+    public int currentLevel = 0;
+
+    public void AdvanceLevel()
+    {
+        if (currentLevel == 0)
+        {
+            this.gameObject.SetActive(true);
+            ++currentLevel;
+        }
+        else
+        {
+            if (nextForms.Count == 0) return;
+            
+                nextForms[currentLevel - 1].AdvanceLevel();
+                ++currentLevel;
+                gameObject.SetActive(false);
+
+        }
+    }
+}
---
+++ Assets/Scripts/HappyTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HappyTree : MonoBehaviour
+{
+    public int _currentLevel = 0;
+    public MasterList values;
+
+    private float _time = 0f;
+    private float advanceTime = 5f;
+
+    public void AdvanceLevel()
+    {
+        if (_currentLevel >= values.list.Count) return;
+        
+        foreach (Advanceable o in values.list[_currentLevel].ContainedList)
+        {
+            o.AdvanceLevel();
+        }
+        _currentLevel++;
+    }
+
+
+    private void Update()
+    {
+        _time = _time + Time.deltaTime;
+        Debug.Log(_time);
+        if (_time > advanceTime)
+        {
+            _time = 0f;
+            AdvanceLevel();
+        }
+    }
+
+    [System.Serializable]
+    public class ListContainer
+    {
+        public List<Advanceable> ContainedList = new List<Advanceable>();
+    }
+    
+    [System.Serializable]
+    public class MasterList
+    {
+        public List<ListContainer> list = new List<ListContainer>();
+    }
+}

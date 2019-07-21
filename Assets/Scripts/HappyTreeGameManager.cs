a358b7df13e4ce55f1eb9afcb759fa0efaf22843
Maja Jabłońska <majajjablonska@gmail.com>
2019-07-21 12:27:32 +0200

    end

--- Assets/Scripts/HappyTreeGameManager.cs
+++ Assets/Scripts/HappyTreeGameManager.cs
@@ -41,7 +41,18 @@ public class HappyTreeGameManager : MonoBehaviour
             //moneyLabel.GetComponent<Text>().text = "Money: " + money;
         }
     }
-    
+
+    public void Update()
+    {
+        GameObject heart = GameObject.FindWithTag("Hearth");
+        if (!heart)
+        {
+            Time.timeScale = 0.0f;
+            GameObject.Find("END").gameObject.SetActiveRecursively(true);
+            //  gameObject.GetChild("childname").SetActive(false);
+        }
+    }
+
     private float buildingSpeedModifier = 1.0f;
     private float towerDamageModifier = 1.0f;
     private float damageReductionModifier = 1.0f;

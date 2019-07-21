56e6bb575b56802f7380fafaba07b3b1d03c096a
agatak8 <shnutzer@gmail.com>
2019-07-21 11:24:32 +0200

    Upgrade WIP

---
+++ Assets/Scripts/Building/TreeUpgradeBuilder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeUpgradeBuilder : MonoBehaviour
+{
+    public TreeUpgradeBuildingSelection buildingSelection;
+    public int upgradeCost;
+    private bool playerInRange;
+    private HappyTreeGameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("HappyTreeGameManager").GetComponent<HappyTreeGameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerInRange)
+        {
+            return;
+        }
+        if (Input.GetButtonDown("ActionButton"))
+        {
+            Build();
+        }
+        else if (Input.GetButtonDown("SelectLeft"))
+        {
+            buildingSelection.prev();
+        }
+        else if (Input.GetButtonDown("SelectRight"))
+        {
+            buildingSelection.next();
+        }
+    }
+
+    private bool CanPlaceBuild()
+    {
+        return gameManager.Money >= upgradeCost; 
+    }
+
+    private void Build()
+    {
+        if (CanPlaceBuild())
+        {
+            gameManager.Money -= upgradeCost;
+            buildingSelection.build();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        buildingSelection.gameObject.SetActive(true);
+        playerInRange = true;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        buildingSelection.gameObject.SetActive(false);
+        playerInRange = false;
+    }
+}
---
+++ Assets/Scripts/Building/TreeUpgradeLine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeUpgradeLine : MonoBehaviour
+{
+    public List<GameObject> branches;
+    public List<SpriteRenderer> previews;
+    private int currentLevel = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public GameObject currentBranch()
+    {
+        return branches[currentLevel];
+    }
+
+    public SpriteRenderer currentBranchPreview()
+    {
+        return previews[currentLevel];
+    }
+
+    public bool IsMaxedOut()
+    {
+        return currentLevel >= branches.Count;
+    }
+
+    public void build()
+    {
+        if (IsMaxedOut())
+        {
+            Debug.Log("Not building, is maxed out");
+            return;
+        }
+        Debug.Log("Building" + currentBranch().name);
+        currentBranchPreview().gameObject.SetActive(false);
+        currentBranch().SetActive(true);
+        currentLevel++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}

15965b47e91430cea5dc2fcc15e311c9369f050c
agatak8 <shnutzer@gmail.com>
2019-07-21 08:52:08 +0200

    Build for filip

--- Assets/Scripts/Enemy/TargetStrategy/GetOneClosestStrategy.cs
+++ Assets/Scripts/Enemy/TargetStrategy/GetOneClosestStrategy.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
-using Object = System.Object;
 
 public class GetOneClosestStrategy : TargetSearchStrategy
 {
